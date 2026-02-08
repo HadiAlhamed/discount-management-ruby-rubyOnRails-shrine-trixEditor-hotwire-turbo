22003caa29cc5730423ced26058db931a0c881cc
Hadi Alhamed <hadialhamed.py@gmail.com>
2026-02-09 00:29:38 +0300

    format files

--- test/system/discounts_test.rb
+++ test/system/discounts_test.rb
@@ -3,6 +3,6 @@ require_relative "../application_system_test_case"
 class DiscountsTest < ApplicationSystemTestCase
   test "visiting the index" do
     visit discounts_url
-    assert_selector "h1", text: "Partner Discount Submission" 
+    assert_selector "h1", text: "Partner Discount Submission"
   end
 end
