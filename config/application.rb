f4804127d0aabc20b1f68f12cc1560e4780281c7
Hadi Alhamed <hadialhamed.py@gmail.com>
2026-02-08 23:55:47 +0300

    try to fix lexy

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module MyBenefitsTask
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
+    config.lexxy.override_action_text_defaults = false
+
 
     # Configuration for the application, engines, and railties goes here.
     #
