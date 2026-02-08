986da962a030904c9ca5504450fd7d1ee5bc6f07
Hadi Alhamed <hadialhamed.py@gmail.com>
2026-02-08 23:55:02 +0300

    add image uploader class to handle image uploades using shrine

---
+++ app/uploaders/image_uploader.rb
@@ -0,0 +1,9 @@
+class ImageUploader < Shrine
+  plugin :validation_helpers
+  plugin :determine_mime_type 
+
+  Attacher.validate do
+    validate_max_size 5*1024*1024, message: "is too large (max 5MB)"
+    validate_mime_type %w[image/jpeg image/png image/webp], message: "must be JPEG, PNG, or WEBP"
+  end
+end
